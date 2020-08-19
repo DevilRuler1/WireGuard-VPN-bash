2c6aaceca013d57e4488c4c9b2c53c5186de060b
Muhammad Moaz <66547583+DevilRuler1@users.noreply.github.com>
2020-08-19 19:58:36 +0500

    Update setup1.sh

--- setup1.sh
+++ setup1.sh
@@ -4,5 +4,5 @@ sudo apt update
 sudo apt install software-properties-common
 sudo add-apt-repository ppa:wireguard/wireguard
 sudo apt install wireguard
-wg genkey | sudo tee /etc/wireguard/privatekey | wg pubkey | sudo tee /etc/wireguard/publickey
-sudo chmod 600 /etc/wireguard/{privatekey,wg0.conf}
+wg genkey | sudo tee /wireguard/privatekey | wg pubkey | sudo tee /wireguard/publickey
+sudo chmod 600 /wireguard/{privatekey,wg0.conf}
