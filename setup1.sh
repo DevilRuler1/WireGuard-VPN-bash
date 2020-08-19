4e825c25468bfe4e379ca1697d9a43a055fac5a6
Muhammad Moaz <66547583+DevilRuler1@users.noreply.github.com>
2020-08-19 19:49:48 +0500

    Update and rename setup.sh to setup1.sh

--- setup1.sh
+++ setup1.sh
@@ -6,8 +6,3 @@ sudo add-apt-repository ppa:wireguard/wireguard
 sudo apt install wireguard
 wg genkey | sudo tee /etc/wireguard/privatekey | wg pubkey | sudo tee /etc/wireguard/publickey
 sudo chmod 600 /etc/wireguard/{privatekey,wg0.conf}
-sudo wg-quick up wg0
-sudo wg show wg0
-sudo systemctl enable wg-quick@wg0
-sudo sysctl -p
-sudo ufw allow 51820/udp
